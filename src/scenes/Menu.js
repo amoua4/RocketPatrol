489dd55da3bd8ddc32982630120528822893214f
Allan <amoua4@ucsc.edu>
2022-04-10 12:31:56 -0700

    audio should work now

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,9 +5,9 @@ class Menu extends Phaser.Scene {
 
     preload() {
         //load audio
-        this.load.audio('sfx_select', '/assets/blip_select.wav');
-        this.load.audio('sfx_explosion', '/assets/explosion9.wav');
-        this.load.audio('sfx_rocket', '/assets/rocket_shot.wav');
+        this.load.audio('sfx_select', './assets/blip_select.wav');
+        this.load.audio('sfx_explosion', './assets/explosion9.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
 
     create() {
